1709759b87486793e13d9f09308fca648a7245cf
Denzel Njuki <denzelnjuki@Denzels-MacBook-Pro.local>
2024-03-18 16:56:28 +0300

    First Commit

---
+++ hack2.py
@@ -0,0 +1,8 @@
+#Prompts a user to enter their age and saves in a variable
+age = int(input("What is your age?"))
+
+#conditional statement to check the age
+if age>= 18 :
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
